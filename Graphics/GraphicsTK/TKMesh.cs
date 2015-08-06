61c3d608b7388aed04db181e5f5f803e15fca736
Tarcontar <michael-schnabel@gmx.net>
2015-08-06 18:21:12 +0200

    add graphics folder

---
+++ Graphics/GraphicsTK/TKMesh.cs
@@ -0,0 +1,33 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SageCS.Graphics
+{
+    public abstract class TKMesh
+    {
+        public Vector3 Position = Vector3.Zero;
+        public Vector3 Rotation = Vector3.Zero;
+        public Vector3 Scale = Vector3.One;
+
+        public int VertCount;
+        public int IndiceCount;
+        public int ColorDataCount;
+        public Matrix4 ModelMatrix = Matrix4.Identity;
+        public Matrix4 ViewProjectionMatrix = Matrix4.Identity;
+        public Matrix4 ModelViewProjectionMatrix = Matrix4.Identity;
+
+        public bool IsTextured = false;
+        public int TextureID;
+        public int TextureCoordsCount;
+  
+        public abstract Vector3[] GetVerts();
+        public abstract int[] GetIndices(int offset = 0);
+        public abstract Vector3[] GetColorData();
+        public abstract void CalculateModelMatrix();
+        public abstract Vector2[] GetTextureCoords();
+    }
+}
---
+++ Graphics/Texture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SageCS.Graphics
+{
+    public abstract class Texture
+    {
+        public static Dictionary<string, int> textures = new Dictionary<string, int>();
+
+        public static int loadImage(Bitmap image)
+        {
+            Console.WriteLine("ERROR: not implemented in Texture.cs");
+            return -1;
+        }
+
+        public static int loadImage(string filename)
+        {
+            try
+            {
+                Bitmap file = new Bitmap(filename);
+                return loadImage(file);
+            }
+            catch (FileNotFoundException e)
+            {
+                return -1;
+            }
+        }
+    }
+}
