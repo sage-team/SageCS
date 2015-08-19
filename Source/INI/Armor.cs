5a429fb5c5d4ffeed640010ec86d91776729dd0d
Tarcontar <michael-schnabel@gmx.net>
2015-08-19 12:50:03 +0200

    added upgrade and armour classes

---
+++ Source/INI/Armor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SageCS.INI
+{
+    public struct Armor
+    {
+        public int DEFAULT;
+        public int FORCE;
+        public int CRUSH;
+        public int SLASH;
+        public int PIERCE;
+        public int SIEGE;
+        public int STRUCTURAL;
+        public int FLAME;
+        public int HEALING;
+        public int UNRESISTABLE;
+        public int WATER;
+        public int PENALTY;
+        public int FALLING;
+        public int TOPPLING;
+        public int REFLECTED;
+        public int PASSENGER;
+        public int MAGIC;
+        public int CHOP;
+        public int HERO;
+        public int SPECIALIST;
+        public int URUK;
+        public int HERO_RANGED;
+        public int FLY_INTO;
+        public int UNDEFINED;
+        public int LOGICAL_FIRE;
+        public int CAVALRY;
+        public int CAVALRY_RANGED;
+        public int POISON;
+    }
+}
---
+++ Source/INI/Upgrade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SageCS.INI
+{
+    public struct Upgrade
+    {
+        public string DisplayName;
+        public string Type;         //PLAYER or OBJECT
+        public string BuildTime;
+        public string BuildCost;
+        public string ButtonImage;
+        public string ResearchSound;
+        public string UnitSpecificSound;
+    }
+}
